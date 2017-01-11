a5164c1bc07bea82126726b73e6ccbaf8c029837
Fitria Mela Agnes <fitria.mela9a17@gmail.com>
2017-01-11 12:57:48 +0700

    hello world

---
+++ hello world/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hello_world.Startup))]
+namespace hello_world
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
